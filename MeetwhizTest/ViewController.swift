f073792ccfdddf3712fef744d3e50388053e7540
DemaGuru <admirdemir@gmail.com>
2016-07-07 15:12:46 +0200

    Initial Commit

---
+++ MeetwhizTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeetwhizTest
+//
+//  Created by Networks Mac1 on 07/07/16.
+//  Copyright © 2016 Networks Mac1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
